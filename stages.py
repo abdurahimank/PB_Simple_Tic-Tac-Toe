bdb7cb83588d1b86e26553ade474d1d8e86227db
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-14 12:09:12 +0530

    Stage 3/5: Guess the age

--- stages.py
+++ stages.py
@@ -1,5 +1,18 @@
-# Stage 2/5: Print your name
-print("""Hello! My name is Aid.
-I was created in 2020.""")
-name = input("Please, remind me your name.\n")
-print(f"What a great name you have, {name}!")
+# Stage 3/5: Guess the age
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+name = input()
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+rem_3 = int(input())
+rem_5 = int(input())
+rem_7 = int(input())
+your_age = (rem_3 * 70 + rem_5 * 21 + rem_7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
