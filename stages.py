6ae7b0796978588cb50d369cf2c74a928c7a0f86
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-14 11:42:10 +0530

    Stage 2/5: Print your name

--- stages.py
+++ stages.py
@@ -1,3 +1,5 @@
-# Stage 1/5: Chatty Bot welcomes you
+# Stage 2/5: Print your name
 print("""Hello! My name is Aid.
 I was created in 2020.""")
+name = input("Please, remind me your name.\n")
+print(f"What a great name you have, {name}!")
