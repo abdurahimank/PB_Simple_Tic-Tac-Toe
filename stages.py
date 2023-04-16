ad341a46fb49a23047759e720b2b0ce151747b7f
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-16 08:33:01 +0530

    Stage 2/5: The user is the gamemaster

--- stages.py
+++ stages.py
@@ -1,4 +1,7 @@
-# Stage 1/5: Welcome to the battlefield!
-print("""X O X
-O X O
-X X O""")
+# Stage 2/5: The user is the gamemaster
+xox = input()
+print(f"""---------
+| {xox[0]} {xox[1]} {xox[2]} |
+| {xox[3]} {xox[4]} {xox[5]} |
+| {xox[6]} {xox[7]} {xox[8]} |
+---------""")
